b7ef8a03f9df6f386ce9a3f24ebd6fc92b4c16fb
josvelema <volumensolutions@gmail.com>
2022-05-26 21:20:39 +0200

    added db con , registration validation and error handling

--- includes/handlers/register-handler.php
+++ includes/handlers/register-handler.php
@@ -1,57 +1,41 @@
-<?php 
+<?php
 
-function sanitizeFormPassword($inputText) {
+function sanitizeFormPassword($inputText)
+{
 	$inputText = strip_tags($inputText);
 	return $inputText;
 }
 
-function sanitizeFormUsername($inputText) {
+function sanitizeFormUsername($inputText)
+{
 	$inputText = strip_tags($inputText);
 	$inputText = str_replace(" ", "", $inputText);
 	return $inputText;
 }
 
-function sanitizeFormString($inputText) {
+function sanitizeFormString($inputText)
+{
 	$inputText = strip_tags($inputText);
 	$inputText = str_replace(" ", "", $inputText);
 	$inputText = ucfirst(strtolower($inputText));
 	return $inputText;
 }
 
-function validateUserName($un) {
 
-}
 
-function validateFirstName($fn) {
-  
-}
-
-function validateLastName($ln) {
-  
-}
-
-function validateEmails($em , $em2) {
-  
-}
-
-function validatePasswords($pw , $pw2) {
-  
-}
-
-if(isset($_POST['registerButton'])) {
+if (isset($_POST['registerButton'])) {
 	//Register button was pressed
 	$username = sanitizeFormUsername($_POST['username']);
-		$firstName = sanitizeFormString($_POST['firstName']);
+	$firstName = sanitizeFormString($_POST['firstName']);
 	$lastName = sanitizeFormString($_POST['lastName']);
 	$email = sanitizeFormString($_POST['email']);
 	$email2 = sanitizeFormString($_POST['email2']);
 	$password = sanitizeFormPassword($_POST['password']);
 	$password2 = sanitizeFormPassword($_POST['password2']);
 
-validateUserName($username);
-validateFirstName($firstName);
-validateLastName($lastName);
-validateEmails($email,$email2);
-validatePasswords($password,$password2);
+	$wasSuccessful = $account->register($username , $firstName , $lastName ,$email , $email2 , $password , $password2);
 
+	if($wasSuccessful) {
+		header("Location: index.php");
+	}
 }
--- register.php
+++ register.php
@@ -1,7 +1,16 @@
 <?php
+include("includes/config.php");
+include("includes/classes/Account.php");
+include("includes/classes/Constants.php");
+$account = new Account($con);
 include("includes/handlers/register-handler.php");
 include("includes/handlers/login-handler.php");
 
+function getInputValue($name) {
+  if(isset($_POST[$name])) {
+    echo $_POST[$name];
+  }
+}
 
  ?>
 
@@ -30,36 +39,55 @@ include("includes/handlers/login-handler.php");
       <button type="submit" name="loginButton">LOG IN</button>
 
     </form>
+
     <form id="registerForm" action="register.php" method="POST">
       <h2>Create your free account</h2>
       <p>
-        <label for="username">Username</label>
-        <input id="username" name="username" type="text" placeholder="Jos" required>
-      </p>
-      <p>
-        <label for="firstname">First Name</label>
-        <input id="firstname" name="firstName" type="text" placeholder="Jos" required>
-      </p>
-      <p>
-        <label for="lastname">Last Name/label>
-          <input id="lastname" name="lastName" type="text" placeholder="Velema" required>
-      </p>
-      <p>
-        <label for="email">E-mail</label>
-        <input id="email" name="email" type="email" placeholder="Jos@codette.net" required>
-      </p>
-      <p>
-        <label for="email2">Confirm E-mail</label>
-        <input id="email2" name="email2" type="text" placeholder="Jos@codette.net" required>
-      </p>
-      <p>
-        <label for="password">Password</label>
-        <input id="password" name="password" type="password" placeholder="your password" required>
-      </p>
-      <p>
-        <label for="password2">Confirm Password</label>
-        <input id="password2" name="password2" type="password" placeholder="confirm your password" required>
-      </p>
+				<?php echo $account->getError(Constants::$usernameCharacters); ?>
+				<?php echo $account->getError(Constants::$usernameTaken); ?>
+
+				<label for="username">Username</label>
+				<input id="username" name="username" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('username'); ?>" required>
+			</p>
+
+			<p>
+				<?php echo $account->getError(Constants::$firstNameCharacters); ?>
+				<label for="firstName">First name</label>
+				<input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" value="<?php getInputValue('firstName'); ?>" required>
+			</p>
+
+			<p>
+				<?php echo $account->getError(Constants::$lastNameCharacters); ?>
+				<label for="lastName">Last name</label>
+				<input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" value="<?php getInputValue('lastName');?>" required>
+			</p>
+
+			<p>
+				<?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+				<?php echo $account->getError(Constants::$emailInvalid); ?>
+				<?php echo $account->getError(Constants::$emailTaken); ?>
+
+				<label for="email">Email</label>
+				<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email'); ?>" required>
+			</p>
+
+			<p>
+				<label for="email2">Confirm email</label>
+				<input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email2'); ?>" required>
+			</p>
+
+			<p>
+				<?php echo $account->getError(Constants::$passwordsDoNoMatch); ?>
+				<?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+				<?php echo $account->getError(Constants::$passwordCharacters); ?>
+				<label for="password">Password</label>
+				<input id="password" name="password" type="password" placeholder="Your password" required>
+			</p>
+
+			<p>
+				<label for="password2">Confirm password</label>
+				<input id="password2" name="password2" type="password" placeholder="Your password" required>
+			</p>
 
       <button type="submit" name="registerButton">REGISTER</button>
 
