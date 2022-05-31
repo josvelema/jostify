eefdea50cd315aa6ece836130a4bf1323a009ffc
josvelema <volumensolutions@gmail.com>
2022-05-31 23:10:52 +0200

    added grid to top container

---
+++ includes/navbar.php
@@ -0,0 +1,32 @@
+<nav id="navBarContainer">
+  <div class="navBar">
+    <a href="index.php" class="logo">
+      <img src="assets/img/jostify.svg" alt="jostify logo">
+    </a>
+
+
+
+    <div class="group">
+      <div class="navItem">
+        <a href="search.php" class="navItemLink">Search
+          <img src="assets/svg/search.svg" alt="search" class="icon"> </a>
+      </div>
+
+
+    </div>
+
+    <div class="group">
+      <div class="navItem">
+        <a href="browse.php" class="navItemLink">Browse</a>
+      </div>
+      <div class="navItem">
+        <a href="yourmusic.php" class="navItemLink">Your music</a>
+      </div>
+      <div class="navItem">
+        <a href="profile.php" class="navItemLink">Profile</a>
+      </div>
+    </div>
+
+  </div>
+
+</nav>
---
+++ includes/nowplayingbar.php
@@ -0,0 +1,85 @@
+<footer id="nowPlayingBarContainer">
+  <div id="nowPlayingBar">
+
+    <div id="nowPlayingLeft">
+      <div class="content">
+
+        <span class="albumLink">
+          <img src="//unsplash.it/200/200" alt="" class="albumArtwork">
+        </span>
+
+        <div class="trackInfo">
+          <span class="trackName">
+            <span>some track name</span>
+          </span>
+          <span class="artistName">
+            <span>artist name</span>
+          </span>
+        </div>
+
+      </div>
+    </div>
+
+    <div id="nowPlayingCenter">
+      <div class="content playerControls">
+        <div class="buttons">
+          <button class="controlButton shuffle" title="shuffle button">
+            <img src="assets/svg/arrows-shuffle-2.svg" alt="shuffle">
+          </button>
+          <button class="controlButton previous" title="previous button">
+            <img src="assets/svg/player-skip-back.svg" alt="previous">
+          </button>
+          <button class="controlButton play" title="play button">
+            <img src="assets/svg/player-play.svg" alt="play">
+          </button>
+          <button class="controlButton pause" title="pause button" style="display: none;">
+            <img src="assets/svg/player-pause.svg" alt="pause">
+          </button>
+          <button class="controlButton next" title="next button">
+            <img src="assets/svg/player-skip-forward.svg" alt="next">
+          </button>
+          <button class="controlButton repeat" title="repeat button">
+            <img src="assets/svg/repeat.svg" alt="repeat">
+          </button>
+          <!-- <button class="controlButton shuffle" title="shuffle button">
+                <img src="assets/svg/" alt="">
+              </button> -->
+
+        </div>
+
+        <div class="playbackBar">
+          <span class="progressTime current">
+            0.00
+          </span>
+          <div class="progressBar">
+            <div class="progressBarBg">
+              <div class="progress"></div>
+            </div>
+          </div>
+          <span class="progressTime remaining">
+            0.00
+          </span>
+        </div>
+
+      </div>
+    </div>
+
+    <div id="nowPlayingRight">
+
+      <div class="volumeBar">
+        <button class="controlButton volume" title="volume button">
+          <img src="assets/svg/volume.svg" alt="Volume">
+        </button>
+
+        <div class="progressBar">
+          <div class="progressBarBg">
+            <div class="progress"></div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+</footer>
